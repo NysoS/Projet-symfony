bda91c93a0347627c43eb75e74ab86cd50443847
Alexis Rosin <rosinalexis@yahoo.fr>
2021-10-07 15:20:31 +0200

    mise a jour

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -18,7 +18,10 @@ use SymfonyCasts\Bundle\ResetPassword\Controller\ResetPasswordControllerTrait;
 use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
 use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
-#[Route('/reset-password')]
+
+/**
+ * @Route("/reset-password")
+ */
 class ResetPasswordController extends AbstractController
 {
     use ResetPasswordControllerTrait;
@@ -33,7 +36,10 @@ class ResetPasswordController extends AbstractController
     /**
      * Display & process form to request a password reset.
      */
-    #[Route('', name: 'app_forgot_password_request')]
+
+    /**
+     * @Route("",name:"app_forgot_password_request" )
+     */
     public function request(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ResetPasswordRequestFormType::class);
@@ -54,7 +60,10 @@ class ResetPasswordController extends AbstractController
     /**
      * Confirmation page after a user has requested a password reset.
      */
-    #[Route('/check-email', name: 'app_check_email')]
+
+    /**
+     * @Route("/check-email",name:"app_check_email" )
+     */
     public function checkEmail(): Response
     {
         // Generate a fake token if the user does not exist or someone hit this page directly.
@@ -71,7 +80,9 @@ class ResetPasswordController extends AbstractController
     /**
      * Validates and process the reset URL that the user clicked in their email.
      */
-    #[Route('/reset/{token}', name: 'app_reset_password')]
+    /**
+     * @Route("/reset/{token}",name:"app_reset_password" )
+     */
     public function reset(Request $request, UserPasswordEncoderInterface $passwordEncoder, string $token = null): Response
     {
         if ($token) {
