7e73a8e4f114fcb32da8bae7253194e5b6c51b6b
Kristofer LEDOUX <kristofer.ledoux2020@campus-eni.fr>
2021-10-07 13:16:44 +0200

    git ignore

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\SitesRepository;
 use App\Repository\SortiesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,14 +14,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(SortiesRepository $sortie, SitesRepository $sites): Response
+    public function index(SortiesRepository $sortie, SitesRepository $sites, Request $req): Response
     {
-        $lstSortie = $sortie->findAll();
+        $lstSortie = $sortie->filtersHomeSorties($req);
         $lstSites = $sites->findAll();
 
+        $memory = [];
+        if($req->get('filter_site') != null){
+            $memory['site'] = $req->get('filter_site');
+        }else if($req->get('filter_site') == null){
+            $memory['site'] = 'default';
+        }
+
+        if($req->get('filter_d1') != null and $req->get('filter_d2') != null){
+            $memory['d1'] = $req->get('filter_d1');
+            $memory['d2'] = $req->get('filter_d2');
+        }
+        
+      //  dd($memory);
         return $this->render('home/index.html.twig', [
             'lstSortie' => $lstSortie,
             'lstSites' => $lstSites,
+            'memory' => $memory,
         ]);
     }
 }
--- src/Repository/SortiesRepository.php
+++ src/Repository/SortiesRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Sorties;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Expr\Value;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @method Sorties|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +19,31 @@ class SortiesRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Sorties::class);
+    }
+
+    public function filtersHomeSorties(Request $req)
+    {
+        $qb = $this->createQueryBuilder('s');
+
+        if ($req->get('filter_site') == 'default') {
+        } else if ($req->get('filter_site') != null) {
+            $qb->where('s.site = :idSite')
+                ->setParameter(':idSite', $req->get('filter_site'));
+        }
+
+        if($req->get('sortie_contains') != null){
+            $qb->where('s.nom like :ns')
+                ->setParameter(':ns','%'.$req->get('sortie_contains').'%');
+        }
+
+        if($req->get('filter_d1') != null and $req->get('filter_d2') != null){
+            $qb->where('s.date_debut >= :d1 and s.date_cloture <= :d2')
+                ->setParameter(':d1',$req->get('filter_d1'))
+                ->setParameter(':d2',$req->get('filter_d2'));
+        }
+
+        $query = $qb->getQuery();
+        return $query->execute();
     }
 
     // /**
